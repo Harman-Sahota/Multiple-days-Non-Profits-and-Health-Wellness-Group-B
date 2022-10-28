ac556e5a0b99d09bd40934f4cb6d5ae665f45696
Harman-Sahota <10harmansahota@gmail.com>
2022-10-28 14:12:42 -0700

    adding the url path access to register page via django

--- multi_non/register/views.py
+++ multi_non/register/views.py
@@ -1,3 +1,8 @@
+import imp
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def register(request):
+    return HttpResponse("<h1>test</h1>")
+    
