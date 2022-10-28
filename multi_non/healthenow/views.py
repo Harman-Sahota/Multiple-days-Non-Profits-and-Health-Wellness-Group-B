2bfa3c7298e6b153c165b2ba44a059720349a543
Harman-Sahota <10harmansahota@gmail.com>
2022-10-28 15:02:30 -0700

    imported bootstrap into the project templates

--- multi_non/healthenow/views.py
+++ multi_non/healthenow/views.py
@@ -1,8 +1,9 @@
 import imp
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def register(request):
-    return render(request, 'healthenow/register.html')
+    return render(request, 'healthenow/register.html', {'title': 'register'})
     
