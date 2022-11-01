f7ff9cd65cc619cc363636f0f9f82d32e56fa22c
Harman-Sahota <10harmansahota@gmail.com>
2022-11-01 09:13:51 -0700

    renaming files to match the new project

--- multi_non/multi_non/urls.py
+++ multi_non/multi_non/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("healthenow/", include('healthenow.urls')),
+    path("foodsaviour/", include('foodsaviour.urls')),
     
 ]
