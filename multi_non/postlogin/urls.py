e59b12ed5752de398e77b37bad6b303ddf08697a
Maysey Lu <mayseylu@hotmail.com>
2022-11-04 15:48:11 -0700

    Initialized other known pages for post-login

--- multi_non/postlogin/urls.py
+++ multi_non/postlogin/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('overview/', views.overview,name='overview')
+    path('overview/', views.overview,name='overview'),
+    path('tracker/', views.tracker,name='tracker'),
+    path('network/', views.network,name='network'),
+    path('profile/', views.profile,name='profile')
 ]
--- multi_non/postlogin/views.py
+++ multi_non/postlogin/views.py
@@ -7,3 +7,12 @@ from django.http import HttpResponse
 # Create your views here.
 def overview(request):
     return render(request, 'postlogin/overview.html', {'title': 'Overview'})
+
+def tracker(request):
+    return render(request, 'postlogin/tracker.html', {'title': 'Tracker'})
+
+def network(request):
+    return render(request, 'postlogin/network.html', {'title': 'Network'})
+
+def profile(request):
+    return render(request, 'postlogin/profile.html', {'title': 'Profile'})
