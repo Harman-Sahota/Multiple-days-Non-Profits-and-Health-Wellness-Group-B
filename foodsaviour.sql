4b7c7f0f75b3742d4c8e84504a79ca551a3e2fb9
Harman-Sahota <10harmansahota@gmail.com>
2022-11-08 11:20:20 -0800

    adding the sql file for you guys

---
+++ foodsaviour.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA foodsaviour;
+USE foodsaviour;
+CREATE TABLE users (
+ FirstName varchar(255),
+ LastName varchar(255),
+ Email varchar(255),
+ Password varchar(255),
+ Roles varchar(255),
+ Consent varchar(50)
+);
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'groupb@2022!';
+GRANT ALL PRIVILEGES ON foodsaviour.* TO 'admin'@'localhost';
+FLUSH PRIVILEGES;
