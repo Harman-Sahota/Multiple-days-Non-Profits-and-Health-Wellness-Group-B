ffe5686fae7de74a7b823141fe0ac224cb714e6e
Harman-Sahota <10harmansahota@gmail.com>
2022-11-09 14:59:38 -0800

    chechboxes working as well as force reload fixed

--- multi_non/foodsaviour/views.py
+++ multi_non/foodsaviour/views.py
@@ -4,17 +4,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from foodsaviour.models import users
 from django.db import connection
-
+ 
 
 def register(request):
     if request.method == "POST":
         newUser = users() 
-        newUser.Roles = request.POST.get("roles")
+        newUser.Roles = str(request.POST.getlist("roles[]"))
         newUser.FirstName = request.POST.get("firstname")
         newUser.LastName = request.POST.get("lastname")
         newUser.Email = request.POST.get("email")
         newUser.Password = request.POST.get("password")
-        newUser.Roles = request.POST.get("roles")
         newUser.Consent = request.POST.get("consent",False)
         cursor = connection.cursor()
         sql = "INSERT INTO users (FirstName,LastName,Email,Password,Roles,Consent) VALUES (%s, %s,%s, %s,%s, %s)"
