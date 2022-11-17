76dfcc4e3624cb6163e04d86d95b28cd58b5a0e1
Harman-Sahota <10harmansahota@gmail.com>
2022-11-17 14:12:36 -0800

    adding admin page v2

--- multi_non/postlogin/urls.py
+++ multi_non/postlogin/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('tracker/', views.tracker,name='tracker'),
     path('network/', views.network,name='network'),
     path('profile/', views.profile,name='profile'),
-    path('admin/', views.profile,name='admin')
+    path('admin/', views.admin,name='admin')
 ]
