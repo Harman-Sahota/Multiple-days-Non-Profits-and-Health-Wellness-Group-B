0ce5466fb295be277376b2fe8b51f409c6838fbf
AbdulazizAlmutlaq0 <almutlaqaziz32@gmail.com>
2022-11-17 16:33:38 +0300

    js validation

---
+++ multi_non/foodsaviour/static/foodsaviour/js/login.js
@@ -0,0 +1,29 @@
+console.log('Login script loaded')
+const emailInputControl = document.getElementById("exampleInputEmail1");
+
+console.log(emailInputControl)
+emailInputControl.addEventListener('change', (e) => {
+    console.log(e.target.value);
+    ValidateEmail(e.target.value);
+})
+function ValidateEmail(input) {
+
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.match(validRegex)) {
+
+    alert("Valid email address!");
+
+    return true;
+
+  } else {
+
+    alert("Invalid email address!");
+
+    emailInputControl.focus();
+
+    return false;
+
+  }
+
+}
