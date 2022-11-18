5a08e71af4e9b89bdedf99e211d5f7f420dffc8e
AbdulazizAlmutlaq0 <almutlaqaziz32@gmail.com>
2022-11-18 20:33:30 +0300

    js validation

--- multi_non/foodsaviour/static/foodsaviour/js/login.js
+++ multi_non/foodsaviour/static/foodsaviour/js/login.js
@@ -1,14 +1,25 @@
 console.log('Login script loaded')
  const emailInputControl = document.getElementById("exampleInputEmail1");
+ const passwordControl = document.getElementById("exampleInputPassword1");
 const sub = document.getElementById("submit");
 
  console.log(emailInputControl)
 
  sub.addEventListener("click", (e) => {
+  e.preventDefault();
   console.log(emailInputControl.value);
   ValidateEmail(emailInputControl.value);
+  ValidatePssword(passwordControl.value);
 })
 
+
+function ValidatePssword(inputPass) {
+    if(inputPass.trim() =="") {
+      passwordControl.style.border = "red 2px solid";
+    }else {
+      passwordControl.style.border = "green 1px solid";
+    }
+}
 
 
  function ValidateEmail(input) {
@@ -17,15 +28,14 @@ const sub = document.getElementById("submit");
 
    if (input.match(validRegex)) {
 
-    emailInputControl.style.borderColor = "none !important";
+    emailInputControl.style.border = "green 1px solid";
 
      return true;
 
    } else {
 
-    emailInputControl.style.borderColor = "red !important";
-    emailInputControl.innerHTML = "invalid";
-     emailInputControl.focus();
+    emailInputControl.style.border = "red 2px solid";
+    emailInputControl.focus();
 
      return false;
 
