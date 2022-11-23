c131b36433dc7b152ba66d8235c5d316081e5e72
Maysey Lu <mayseylu@hotmail.com>
2022-11-23 12:43:13 -0800

    Created landfill calculator function

---
+++ multi_non/postlogin/static/postlogin/js/tracker.js
@@ -0,0 +1,12 @@
+const clients = document.getElementById("clients");
+const afeed = document.getElementById("animalfeed");
+const compost = document.getElementById("compost/fert");
+const partNet = document.getElementById("partnet");
+const landfill = document.getElementById('landfill');
+
+function calculateLandfill(){
+  const sum = Number(clients.value) + Number(afeed.value) + Number(compost.value) + Number(partNet.value);
+  const diff = 100 - Number(sum);
+
+  landfill.value = diff;
+}
