bb61ba63750f3ade39fb71cfd141b1c4c2d65dd5
Maysey Lu <mayseylu@hotmail.com>
2023-01-16 21:18:41 -0800

    Updated resetPassword CSS references

--- frontend/src/pages/resetPassword.js
+++ frontend/src/pages/resetPassword.js
@@ -9,26 +9,25 @@ function resetPassword() {
     return (
         <>
             <section>
-                <div className="body">
-                    <div className="form-container">
-                        <Form className="form mx-auto bg-white">
-                            <h5 className="form-title">Reset Password</h5>
-
+                <div className={resetCSS.body}>
+                    <div className={resetCSS.form-container}>
+                        <Form className={`form mx-auto bg-white ${resetCSS.form}`}>
+                            <h5 className={`form-title ${resetCSS.form-title}`}>Reset Password</h5>
                             <div>
                                 <span>
-                                    <p className="form-title-text mb-0">
+                                    <p className={`form-title-text mb-0`}>
                                         If the details entered ascribe to an account, your password will be
                                         reset.
                                     </p>
                                 </span>
                             </div>
 
-                            <div className="form-floating email-form">
+                            <div className={`form-floating email-form ${resetCSS.email-form}`}>
                                 <Form.Control type="email" id="exampleInputEmail1" className="form-control input-text" placeholder="jordan@gmail.com" name="email"/>
                                 <Form.Label for="exampleInputEmail1" className="form-label input-text">Email:</Form.Label>
                             </div>
 
-                            <Button classname="resetPassword btn btn-outline-danger reset-button" variant="outline-danger" type="reset">Reset Password</Button>
+                            <Button classname={`resetPassword btn btn-outline-danger reset-button ${resetCSS.reset-button}`} variant="outline-danger" type="reset">Reset Password</Button>
                         </Form>
 
                     </div>
